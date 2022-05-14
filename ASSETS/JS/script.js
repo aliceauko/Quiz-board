5911f4bf1475d283c3a7cbd309bc172156ac7c88
aliceauko <alice.auko@student.moringaschool.com>
2022-05-14 12:12:40 +0300

    Edit the questions to be h1 and remove the input elements from the paragraphs

---
+++ ASSETS/JS/script.js
@@ -0,0 +1,16 @@
+function check() {
+    var c=0;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+    if (q1=="Object oriented") {c++}
+    if (q2=="Both A and B") {c++}
+    if (q3=="const") {c++}
+    if (q4=="in") {c++}
+    if (q5=="Object") {c++}
+
+       document.write(c)
+    
+}
