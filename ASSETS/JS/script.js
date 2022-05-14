a52735eff098dfdb4ef2e13b06b31a0890ec438a
aliceauko <alice.auko@student.moringaschool.com>
2022-05-14 20:21:13 +0300

    Add the interactivity of the forms in order to get results

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,16 +1,26 @@
-function check() {
-    var c=0;
-    var q1=document.quiz.question1.value;
-    var q2=document.quiz.question2.value;
-    var q3=document.quiz.question3.value;
-    var q4=document.quiz.question4.value;
-    var q5=document.quiz.question5.value;
-    if (q1=="Object oriented") {c++}
-    if (q2=="Both A and B") {c++}
-    if (q3=="const") {c++}
-    if (q4=="in") {c++}
-    if (q5=="Object") {c++}
-
-       document.write(c)
+function check(){
+    var score=0;
+    if (document.getElementById('correct1').checked) 
+    {
+       score++;
+    }
+    if (document.getElementById('correct2').checked) 
+    {
+       score ++;
+    }
+    if (document.getElementById('correct3').checked) 
+    {
+        score ++;
+    }
+    if (document.getElementById('correct4').checked) 
+    {
+        score ++;
+    }
+    if (document.getElementById('correct5').checked) 
+    {
+        score ++;
+    }
+    document.write("Your score is:"+score);
     
+
 }
